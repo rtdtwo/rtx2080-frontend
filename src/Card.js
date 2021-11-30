d3a3214d0677b744ec9e7acd76e7e882c978eee7
bhavya-kavdia <bhavya.kavdia97@gmail.com>
2021-11-30 15:21:32 -0500

    Recipe tab changes

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
 
 function Card(props) {
+
     const dragStart = e => {
         const target = e.target;
         e.dataTransfer.setData('card_id', target.id);
@@ -13,13 +15,22 @@ function Card(props) {
     const dragOver = e => {
         e.stopPropagation();
     }
+
+    const handleEditRecipe = props => {
+        console.log("##On selecting a recipe to edit--", props);
+        console.log("##On selecting a recipe to edit ID--", props.id);
+        console.log("##On selecting a recipe to edit Name--", props.name);
+        
+    }
     return (
+        
         <div id={props.id} onDragStart={dragStart} onDragOver={dragOver} draggable={props.draggable} className={props.className}>
-            <h5 className="mt-2 text-center">Service {props.id}</h5>
+            <h5 className="mt-2 text-center">{props.name}</h5>
             <div className="col-md-5 text-start pt-2">
-                <p><i className="bi bi-caret-right-fill"></i> <strong>ID: </strong>{props.id}</p>
-                <p><i className="bi bi-caret-right-fill"></i> <strong>Name: </strong>{props.name}</p>
-                {/* {props.showDesc ? (<p><i className="bi bi-caret-right-fill"></i> <strong>Description: </strong>{props.desc}</p>) : ""} */}
+                <p><i className="bi bi-caret-right-fill"></i> <strong>ID : </strong>{props.id}</p>
+                <p><i className="bi bi-caret-right-fill"></i> <strong>Name : </strong>{props.name}</p>
+                {props.isRecipeTab ? (<p><i className="bi bi-caret-right-fill"></i> <strong>No of Relationships : </strong>{props.relSize}</p>) : ""}
+                
                 
             </div>
             {props.children}
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,48 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { useState, useEffect } from 'react';
+import Card from './Card'
+import ModalComponent from './ModalComponent'
+import { Container, Dropdown, Row, Col, Modal, Button } from 'react-bootstrap';
 
 export default function Recipe() {
+
+    const [show, setShow] = useState(false);
+    // const relationdata = [{"id":1,"name":"relationship1","service1":{"id":1,"thingId":1,"name":"Service1","icon":"","desc":""},"service2":{"id":2,"thingId":2,"name":"Service2","icon":"","desc":""}},{"id":2,"name":"relationship2","service1":{"id":2,"thingId":2,"name":"Service2","icon":"","desc":""},"service2":{"id":3,"thingId":3,"name":"Service3","icon":"","desc":""}},{"id":3,"name":"relationship3","service1":{"id":3,"thingId":3,"name":"Service3","icon":"","desc":""},"service2":{"id":4,"thingId":4,"name":"Service4","icon":"","desc":""}}];
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    let selectedRelationship = null; 
+    const relationdata = [{"id":1,"name":"relationship1","service1":{"id":1,"thingId":1,"name":"Service1","icon":"","desc":""},"service2":{"id":2,"thingId":2,"name":"Service2","icon":"","desc":""}},{"id":2,"name":"relationship2","service1":{"id":2,"thingId":2,"name":"Service2","icon":"","desc":""},"service2":{"id":3,"thingId":3,"name":"Service3","icon":"","desc":""}},{"id":3,"name":"relationship3","service1":{"id":3,"thingId":3,"name":"Service3","icon":"","desc":""},"service2":{"id":4,"thingId":4,"name":"Service4","icon":"","desc":""}}];
+    console.log(relationdata[0])
+    const lstExistingRecipes = [{"id":1,"name":"Recipe 1","Relationships":[{"id":1,"name":"relationship1","service1":{"id":1,"thingId":1,"name":"Service1","icon":"","desc":""},"service2":{"id":2,"thingId":2,"name":"Service2","icon":"","desc":""}},{"id":2,"name":"relationship2","service1":{"id":3,"thingId":3,"name":"Service3","icon":"","desc":""},"service2":{"id":4,"thingId":4,"name":"Service4","icon":"","desc":""}},{"id":3,"name":"relationship3","service1":{"id":4,"thingId":4,"name":"Service4","icon":"","desc":""},"service2":{"id":5,"thingId":5,"name":"Service5","icon":"","desc":""}},
+                                                                    {"id":4,"name":"relationship4","service1":{"id":5,"thingId":5,"name":"Service5","icon":"","desc":""},"service2":{"id":6,"thingId":6,"name":"Service6","icon":"","desc":""}},{"id":5,"name":"relationship5","service1":{"id":7,"thingId":7,"name":"Service7","icon":"","desc":""},"service2":{"id":8,"thingId":8,"name":"Service8","icon":"","desc":""}},{"id":6,"name":"relationship6","service1":{"id":9,"thingId":9,"name":"Service9","icon":"","desc":""},"service2":{"id":10,"thingId":10,"name":"Service10","icon":"","desc":""}}]},
+                                {"id":2,"name":"Recipe 2","Relationships":[{"id":1,"name":"relationship1","service1":{"id":1,"thingId":1,"name":"Service1","icon":"","desc":""},"service2":{"id":2,"thingId":2,"name":"Service2","icon":"","desc":""}},{"id":2,"name":"relationship2","service1":{"id":3,"thingId":3,"name":"Service3","icon":"","desc":""},"service2":{"id":4,"thingId":4,"name":"Service4","icon":"","desc":""}},{"id":3,"name":"relationship3","service1":{"id":4,"thingId":4,"name":"Service4","icon":"","desc":""},"service2":{"id":5,"thingId":5,"name":"Service5","icon":"","desc":""}},
+                                                                    {"id":4,"name":"relationship4","service1":{"id":5,"thingId":5,"name":"Service5","icon":"","desc":""},"service2":{"id":6,"thingId":6,"name":"Service6","icon":"","desc":""}}]},
+                                {"id":3,"name":"Recipe 3","Relationships":[{"id":1,"name":"relationship1","service1":{"id":1,"thingId":1,"name":"Service1","icon":"","desc":""},"service2":{"id":2,"thingId":2,"name":"Service2","icon":"","desc":""}},{"id":2,"name":"relationship2","service1":{"id":3,"thingId":3,"name":"Service3","icon":"","desc":""},"service2":{"id":4,"thingId":4,"name":"Service4","icon":"","desc":""}},{"id":3,"name":"relationship3","service1":{"id":4,"thingId":4,"name":"Service4","icon":"","desc":""},"service2":{"id":5,"thingId":5,"name":"Service5","icon":"","desc":""}},
+                                                                    {"id":4,"name":"relationship4","service1":{"id":5,"thingId":5,"name":"Service5","icon":"","desc":""},"service2":{"id":6,"thingId":6,"name":"Service6","icon":"","desc":""}},{"id":5,"name":"relationship5","service1":{"id":7,"thingId":7,"name":"Service7","icon":"","desc":""},"service2":{"id":8,"thingId":8,"name":"Service8","icon":"","desc":""}}]}
+                                       ];
+    const handleEditRecipe = (relJsonData, e)=> {
+        selectedRelationship = [];
+        console.log("##On selecting a recipe to edit--", relJsonData);
+        selectedRelationship.push(relJsonData)
+        setShow(true);
+    }
     return (
         <div>
             <Container>
-                Recipe
+                <Button onClick={handleShow}>Create Recipe</Button>
+
+                {/* <h4>You selected {value}</h4> */}
+                {lstExistingRecipes.map(function (i) {
+                    let intRelSize = i.Relationships.length;
+                    console.log("## i size - ", intRelSize);
+                    return (
+                        <Card id={i.id} name={i.name} className='servicecard' relSize={intRelSize} showDesc='false' isRecipeTab = "true" draggable='false'>
+                                                    <div><Button onClick={(e) => handleEditRecipe(i, e)}>Edit</Button></div>
+
+                        </Card>
+                        
+                    )
+                })}
             </Container>
         </div>
     )
